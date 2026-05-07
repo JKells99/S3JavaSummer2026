1b638ce19a618df030d7cbb8099784e159f01109
Jordan Kelloway <106637327+JKells99@users.noreply.github.com>
2026-05-07 10:21:42 -0230

    add basic menu and inventory display to Store class

--- src/musicstore/Store.java
+++ src/musicstore/Store.java
@@ -1,6 +1,29 @@
 package musicstore;
 
+import java.util.Scanner;
+
 public class Store {
-    // THIS WILL BE THE MAIN CLASS FOR YOUR APPLICATION. YOU CAN CREATE OTHER CLASSES AS NEEDED.
+    static void main() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome to the Music Store!");
+        System.out.println("Please select an option:");
+        System.out.println("1. View Inventory");
+        int choice = scanner.nextInt();
+        switch (choice) {
+            case 1:
+                displayInventory(scanner);
+                break;
+            default:
+                System.out.println("Invalid option. Please try again.");
+        }
+    }
+
+    private static void displayInventory(Scanner scanner) {
+        System.out.println("Inventory:");
+        System.out.println("1. Guitar - $500");
+        System.out.println("2. Piano - $2000");
+        System.out.println("3. Drums - $800");
+    }
 
 }
